2b09ef39703f9ade0ab68c52acbed646e97370a6
Woncheol Kim <icijiy@gmail.com>
2023-04-14 15:09:50 -0700

    Updated Navbar with Logo image

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { Autour_One } from "next/font/google";
+
 interface ContainerProps{
     children: React.ReactNode;
 }
@@ -6,7 +10,18 @@ const Container: React.FC<ContainerProps> = ({
     children
 }) => {
     return (
-        <div>{children}</div>
+        <div
+            className="
+                max-w-[2520px]
+                mx-auto
+                xl:px-20
+                md:px-10
+                sm:px-2
+                px-4
+            "
+            >
+            {children}
+            </div>
     )
 }
 
