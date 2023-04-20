7230bbd16564293fcb518c3a60cab939dac1e32e
Woncheol Kim <icijiy@gmail.com>
2023-04-20 08:14:38 -0700

    Added Modal file

--- layout.tsx
+++ layout.tsx
@@ -2,6 +2,8 @@ import { Nunito } from "next/font/google";
 
 import './globals.css'
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modal/Modal";
 
 export const metadata = {
   title: 'Airbnb',
@@ -20,8 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="{font.className}">
-        <Navbar />
-        {children}</body>
+          <ClientOnly>
+            <Modal />
+            <Navbar />
+          </ClientOnly>
+          {children}
+        </body>
     </html>
   )
 }
