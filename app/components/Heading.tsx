6f30cd7568ee19f73dd39cc8f6b1cdd909304907
Woncheol Kim <icijiy@gmail.com>
2023-05-03 17:50:30 -0700

    Implemented google auth

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -11,16 +11,16 @@ const Heading: React.FC<HeadingProps> = ({
     subtitle,
     center
 }) => {
-    return (
+    return ( 
         <div className={center ? 'text-center' : 'text-start'}>
-            <div className="text-2xl font-bold">
-                {title}
-            </div>
-            <div className="font-light text-neutral-500 mt-2">
-                {subtitle}
-            </div>
+          <div className="text-2xl font-bold">
+            {title}
+          </div>
+          <div className="font-light text-neutral-500 mt-2">
+            {subtitle}
+          </div>
         </div>
-    );
-}
+       );
+    }
 
 export default Heading;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,11 +7,11 @@ import MenuItem from './MenuItem';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { User } from '@prisma/client';
 import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
 interface UserMEnuProps {
-    currentUser?: User | null
+    currentUser?: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMEnuProps> = ({
@@ -64,7 +64,7 @@ const UserMenu: React.FC<UserMEnuProps> = ({
                     >
                     <AiOutlineMenu />
                     <div className='hidden md:block'>
-                        <Avatar />
+                        <Avatar src={currentUser?.image} />
                     </div>
                 </div>
             </div>
