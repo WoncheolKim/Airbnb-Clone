8ed671f6eb75de972906004ef4980b902445ee07
Woncheol Kim <icijiy@gmail.com>
2023-05-04 13:17:03 -0700

    Installed query-string

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import Container from "../Container";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+
 import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
@@ -38,6 +40,7 @@ const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
     );
 }
